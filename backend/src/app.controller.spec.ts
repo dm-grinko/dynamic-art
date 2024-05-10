e4be33ea334bfba45304db66f414214bdbfa9274
Dmitry Grinko <dm.grinko@gmail.com>
2024-05-10 00:08:29 -0700

    init

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -35,7 +35,7 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
+  describe('health endpoint', () => {
     it('should return "OK"', () => {
       expect(appController.health()).toBe('OK');
     });
